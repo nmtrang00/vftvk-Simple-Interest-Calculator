efeeeef427f62ab93d1836eb8c3f0921514967fd
Trang <nmt831200@gmail.com>
2022-01-14 17:30:48 +0700

    Validate before computing

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function compute()
 {
+    // Validate principal
+    if (validate_value()) {
     // To compute the interest
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,<br/>at an interest rate of <mark>"+rate+"</mark>%.<br/>You will receive an amount of <mark>"+interest+"</mark>,<br/> in the year <mark>"+year+"</mark>";
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,<br/>at an interest rate of <mark>"+rate+"</mark>%.<br/>You will receive an amount of <mark>"+interest+"</mark>,<br/> in the year <mark>"+year+"</mark>";
+    }
 }
 
 function updateRate() 
@@ -22,5 +25,9 @@ function validate_value()
     var p = document.getElementById("principal").value;
     if (p<=0){
         alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else{
+        return true
     }
 }
